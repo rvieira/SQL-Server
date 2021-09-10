d7b8d13569931012ff314f00f1a8fc5e70ecd005
rvieira <ricardo@rvieira.co.uk>
2021-09-10 11:43:12 +0200

    modified:   restore-progress.sql

--- restore-progress.sql
+++ restore-progress.sql
@@ -1,5 +1,14 @@
 -- Shows the progress of backup and restore operations
  
-SELECT session_id as SPID, command, a.text AS Query, start_time, percent_complete, dateadd(second,estimated_completion_time/1000, getdate()) as estimated_completion_time 
-FROM sys.dm_exec_requests r CROSS APPLY sys.dm_exec_sql_text(r.sql_handle) a 
-WHERE r.command in ('BACKUP DATABASE','RESTORE DATABASE')
+SELECT 
+    session_id as SPID, 
+    command, 
+    a.text AS Query, 
+    start_time, 
+    percent_complete, 
+    dateadd(second,estimated_completion_time/1000, getdate()) as estimated_completion_time 
+FROM 
+    sys.dm_exec_requests r CROSS APPLY 
+    sys.dm_exec_sql_text(r.sql_handle) a 
+WHERE 
+    r.command in ('BACKUP DATABASE','RESTORE DATABASE')
